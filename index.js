f6cbaddfb9bbc8ed5a3291506066f527232e4b30
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-04-01 19:15:23 -0400

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+
+function superbowlWin(record){
+    
+   for( let item of record)
+   {
+      if(item.result === 'W')
+      {
+          return item.year;
+      }
+   }
+
+}
+
+record.find(superbowlWin);
